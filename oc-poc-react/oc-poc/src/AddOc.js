92bd1b363ec95ca869ab0fa637ce9c3c1626dc83
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-02-19 17:30:59 +0100

    Added AddOc component

---
+++ oc-poc-react/oc-poc/src/AddOc.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class AddOc extends Component {
+  state = {
+    id: null,
+    name: null,
+    ref: null
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="id">Id</label>
+          <input type="text" id="id" onChange={this.handleChange} />
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" onChange={this.handleChange} />
+          <label htmlFor="ref">Reference</label>
+          <input type="text" id="ref" onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddOc;
--- oc-poc-react/oc-poc/src/App.js
+++ oc-poc-react/oc-poc/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import OcComponent from './OcComponent';
+import AddOc from './AddOc';
 
 class App extends Component {
   state = {
@@ -16,6 +17,7 @@ class App extends Component {
         <h1>My first React app!</h1>
         <p>Welcome ;)</p>
         <OcComponent ocs={this.state.ocs} />
+        <AddOc />
       </div>
     );
   }
