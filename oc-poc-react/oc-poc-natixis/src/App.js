e8d3e770056417c0cccedba6b0b50d877a8e54d8
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-03-11 17:11:13 +0100

    Added react router

--- oc-poc-react/oc-poc-natixis/src/App.js
+++ oc-poc-react/oc-poc-natixis/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as BRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <BRouter>
+        <div className="App"></div>
+      </BRouter>
+    );
+  }
 }
 
 export default App;
