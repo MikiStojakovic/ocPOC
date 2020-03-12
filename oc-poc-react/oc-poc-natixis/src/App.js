697175d9577829b8e9fc4545f41190221d30265e
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-03-12 10:18:06 +0100

    Fixed materialize css links

--- oc-poc-react/oc-poc-natixis/src/App.js
+++ oc-poc-react/oc-poc-natixis/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as BRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <BRouter>
-        <div className="App"></div>
+        <div className="App">
+          <Navbar />
+        </div>
       </BRouter>
     );
   }
