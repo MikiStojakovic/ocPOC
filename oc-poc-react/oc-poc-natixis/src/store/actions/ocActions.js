47148fd960eb41604e70c6bb742beb4d7db04e4e
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-03-16 12:26:36 +0100

    Added firebase and firestore set up

--- oc-poc-react/oc-poc-natixis/src/store/actions/ocActions.js
+++ oc-poc-react/oc-poc-natixis/src/store/actions/ocActions.js
@@ -1,5 +1,5 @@
 export const createOc = oc => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //async call to db
     dispatch({ type: 'CREATE_OC', oc });
   };
