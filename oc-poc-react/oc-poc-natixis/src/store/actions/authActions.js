666886606b619cd5b913efb027ce0dadc9c03b73
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-03-23 15:45:25 +0100

    Added login functionality

---
+++ oc-poc-react/oc-poc-natixis/src/store/actions/authActions.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const signIn = credentials => {
+  return (dispatch, getState) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: 'LOGIN_SUCCESS' });
+      })
+      .catch(err => {
+        dispatch({ type: 'LOGIN_ERROR', err });
+      });
+  };
+};
