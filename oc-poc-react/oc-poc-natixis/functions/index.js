7afadcac8c7d22f3f183804c764cbb6339d6f803
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-03-26 17:10:35 +0100

    Added createOc notification cloud function

--- oc-poc-react/oc-poc-natixis/functions/index.js
+++ oc-poc-react/oc-poc-natixis/functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
+});
+
+const createNotification = nofitication => {
+  return admin
+    .firestore()
+    .collection('notifications')
+    .add(nofitication)
+    .then(doc => console.log('notification added', doc));
+};
+
+exports.ocCreated = functions.firestore.document('ocs/{ocId}').onCreate(doc => {
+  const oc = doc.data();
+  const nofitication = {
+    content: 'Added new oc',
+    user: `${oc.conseillerFirstName} ${oc.conseillerLastName}`,
+    time: admin.firestore.FieldValue.serverTimestamp()
+  };
+
+  return createNotification(nofitication);
 });
