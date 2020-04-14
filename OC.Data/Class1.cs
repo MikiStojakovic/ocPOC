fd53208ea8f9f9d2c88fa0e9c1dcba3066cf7bf9
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-14 12:34:42 +0200

    Added NatixisDbContext

--- OC.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OC.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ OC.Data/NatixisDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OC.Core.Models;
+using OC.Data.Configurations;
+
+namespace OC.Data
+{
+    public class NatixisDbContext : DbContext
+    {
+        public DbSet<OuvertureCompte> OuvertureComptes { get; set; }
+        public DbSet<OuvertureCompteDetails> OuvertureCompteDetails { get; set; }
+        public DbSet<Conseiller> Conseillers { get; set; }
+        public DbSet<Fourisseur> Fourisseurs { get; set; }
+
+        public NatixisDbContext(DbContextOptions<NatixisDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .ApplyConfiguration(new OuvertureCompteConfiguration());
+
+            builder
+                .ApplyConfiguration(new OuvertureCompteDetailsConfiguration());
+
+            builder
+                .ApplyConfiguration(new ConseillerConfiguration());
+
+            builder
+                .ApplyConfiguration(new FourisseurConfiguration());
+        }
+    }
+}
