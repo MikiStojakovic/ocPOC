58263af7b9367ee099c08335898cc2b0fb98f0dc
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-16 10:36:12 +0200

    Added Prospect model and repository, sevice interfaces

---
+++ OC.Core/Models/Prospect.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OC.Core.Models
+{
+    public class Prospect
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid ConseillerId { get; set; }
+    }
+}
---
+++ OC.Core/Repositories/IProspectRepository.cs
@@ -0,0 +1,18 @@
+﻿using OC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OC.Core.Repositories
+{
+    public interface IProspectRepository
+    {
+        Task<IEnumerable<Prospect>> GetAllProspect();
+        Task<Prospect> GetProspectById(Guid id);
+        Task<IEnumerable<Prospect>> GetAllProspectByConseillerId();
+        Task<Prospect> Create(Prospect prospect);
+        Task<bool> Delete(string id);
+        void Update(string id, Prospect prospect);
+    }
+}
