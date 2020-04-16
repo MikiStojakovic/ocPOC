6071e2fe547bc8a833ff8b6620715d4da79d16d6
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-16 11:52:22 +0200

    Added MongoDB configuration

---
+++ OC.Data.MongoDB/Setting/DatabaseSetting.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using OC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OC.Data.MongoDB.Setting
+{
+    public class DatabaseSetting : IDatabaseSettings
+    {
+        private readonly IMongoDatabase _db;
+
+        public DatabaseSetting(IOptions<Settings> options, IMongoClient client)
+        {
+            _db = client.GetDatabase(options.Value.Database);
+        }
+
+        public IMongoCollection<Prospect> Prospects => _db.GetCollection<Prospect>("Prospects");
+    }
+}
---
+++ OC.Data.MongoDB/Setting/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using OC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OC.Data.MongoDB.Setting
+{
+    public interface IDatabaseSettings
+    {
+        IMongoCollection<Prospect> Prospects { get;  }
+    }
+}
