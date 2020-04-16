5a6a87dd68496243a74beba86a32101cbb0b6cd7
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-16 12:30:14 +0200

    Integrated MongoDB middleware to API proj

--- NatixisOC/Startup.cs
+++ NatixisOC/Startup.cs
@@ -11,8 +11,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using OC.Core.UnitOfWork;
 using OC.Data;
+using OC.Data.MongoDB.Setting;
 
 namespace NatixisOC
 {
@@ -29,8 +31,19 @@ namespace NatixisOC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //Sql Server Configuration 
             services.AddDbContext<NatixisDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevNatixisOdc"), x => x.MigrationsAssembly("OC.Data")));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //MongoDB Configuration
+            services.Configure<Settings>(
+                options =>
+                {
+                    options.ConnectionString = Configuration.GetValue<string>("MongoDB:ConnectionString");
+                    options.Database = Configuration.GetValue<string>("MongoDB:Database");
+                });
+            services.AddSingleton<IMongoClient, MongoClient>(
+                x => new MongoClient(Configuration.GetValue<string>("MongoDB:ConnectionString")));
+            services.AddTransient<IDatabaseSettings, DatabaseSetting>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
