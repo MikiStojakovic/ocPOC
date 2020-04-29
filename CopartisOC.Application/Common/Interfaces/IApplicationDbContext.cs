613ebb74c6c03a880aa493e0290a43793fac1b1d
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-29 16:41:45 +0200

    Added IApplicationDbContext interface

---
+++ CopartisOC.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CopartisOC.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Conseiller> Conseillers { get; set; }
+        DbSet<Fourisseur> Fourisseurs { get; set; }
+        DbSet<OuvertureCompte> OuvertureComptes { get; set; }
+        DbSet<Prospect> Prospects { get; set; }
+        
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
