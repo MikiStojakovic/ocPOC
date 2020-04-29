fb38fdda26d57c265af277e5c3e7a9fa1344b610
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-29 16:34:53 +0200

    Added IIdentityService interface

---
+++ CopartisOC.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopartisOC.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+
+        //Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+
+        //Task<Result> DeleteUserAsync(string userId);
+    }
+}
