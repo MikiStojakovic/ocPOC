df40d811e2b228f4d9d216938f2247d755f30dd8
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-30 16:16:06 +0200

    Minor changes

--- CopartisOC.Application/Common/Behaviours/LoggingBehaviour.cs
+++ CopartisOC.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using MediatR.Pipeline;
+﻿using CopartisOC.Application.Common.Interfaces;
+using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,32 +12,30 @@ namespace CopartisOC.Application.Common.Behaviours
     public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
     {
         private readonly ILogger _logger;
-        //private readonly ICurrentUserService _currentUserService;
-        //private readonly IIdentityService _identityService;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IIdentityService _identityService;
 
-        public LoggingBehaviour(ILogger<TRequest> logger
-            //, ICurrentUserService currentUserService, IIdentityService identityService
+        public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService, IIdentityService identityService
             )
         {
             _logger = logger;
-            //_currentUserService = currentUserService;
-            //_identityService = identityService;
+            _currentUserService = currentUserService;
+            _identityService = identityService;
         }
 
         public async Task Process(TRequest request, CancellationToken cancellationToken)
         {
             var requestName = typeof(TRequest).Name;
-            //var userId = _currentUserService.UserId ?? string.Empty;
+            var userId = _currentUserService.UserId ?? string.Empty;
             string userName = string.Empty;
 
-            //if (!string.IsNullOrEmpty(userId))
-            //{
-            //    userName = await _identityService.GetUserNameAsync(userId);
-            //}
+            if (!string.IsNullOrEmpty(userId))
+            {
+                userName = await _identityService.GetUserNameAsync(userId);
+            }
 
             _logger.LogInformation("CopartisOC Request: {Name} {@UserId} {@UserName} {@Request}",
-                requestName, "userId todo", "userName todo", request);
-            //requestName, userId, userName, request);
+                requestName, userId, userName, request);
         }
     }
 }
--- CopartisOC.Application/Common/Behaviours/PerformanceBehaviour.cs
+++ CopartisOC.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using CopartisOC.Application.Common.Interfaces;
+using MediatR;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,21 +14,20 @@ namespace CopartisOC.Application.Common.Behaviours
     {
         private readonly Stopwatch _timer;
         private readonly ILogger<TRequest> _logger;
-        //private readonly ICurrentUserService _currentUserService;
-        //private readonly IIdentityService _identityService;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IIdentityService _identityService;
 
         public PerformanceBehaviour(
-            ILogger<TRequest> logger
-            //,
-            //ICurrentUserService currentUserService,
-            //IIdentityService identityService
+            ILogger<TRequest> logger,
+            ICurrentUserService currentUserService,
+            IIdentityService identityService
             )
         {
             _timer = new Stopwatch();
 
             _logger = logger;
-            //_currentUserService = currentUserService;
-            //_identityService = identityService;
+            _currentUserService = currentUserService;
+            _identityService = identityService;
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
@@ -43,17 +43,17 @@ namespace CopartisOC.Application.Common.Behaviours
             if (elapsedMilliseconds > 500)
             {
                 var requestName = typeof(TRequest).Name;
-                //var userId = _currentUserService.UserId ?? string.Empty;
+                var userId = _currentUserService.UserId
+                    ?? string.Empty;
                 var userName = string.Empty;
 
-                //if (!string.IsNullOrEmpty(userId))
-                //{
-                //    userName = await _identityService.GetUserNameAsync(userId);
-                //}
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    userName = await _identityService.GetUserNameAsync(userId);
+                }
 
                 _logger.LogWarning("CopartisOC Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@UserName} {@Request}",
-                        requestName, elapsedMilliseconds, "userId todo", "userName todo", request);
-                //requestName, elapsedMilliseconds, userId, userName, request);
+                        requestName, elapsedMilliseconds, userId, userName, request);
             }
 
             return response;
