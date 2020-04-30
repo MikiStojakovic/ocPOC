e2e0a615ecd6f77c253cc9b8a2698a8213d5664c
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-04-30 17:05:41 +0200

    Added CreateConseillerCommand and CreateConseillerCommandHandler

--- CopartisOC.Application/Conseiller/Commands/CreateConseiller/CreateConseillerCommand.cs
+++ CopartisOC.Application/Conseiller/Commands/CreateConseiller/CreateConseillerCommand.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CopartisOC.Application.Conseiller.Commands.CreateConseiller
 {
-    public class CreateConseillerCommand
+    public class CreateConseillerCommand : IRequest<Guid>
     {
+        public string Code { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public Guid FourisseurId { get; set; }
     }
 }
---
+++ CopartisOC.Application/Conseiller/Commands/CreateConseiller/CreateConseillerCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using CopartisOC.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CoreDomainEntities = CopartisOC.Domain.Entities;
+
+namespace CopartisOC.Application.Conseiller.Commands.CreateConseiller
+{
+    public class CreateConseillerCommandHandler : IRequestHandler<CreateConseillerCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+        public CreateConseillerCommandHandler(IApplicationDbContext context)
+        {
+
+        }
+
+        public async Task<Guid> Handle(CreateConseillerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new CoreDomainEntities.Conseiller
+            {
+                Id = Guid.NewGuid(),
+                Code = request.Code,
+                Nom = request.Nom,
+                Prenom = request.Prenom,
+                FourisseurId = request.FourisseurId
+            };
+
+            _context.Conseillers.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
