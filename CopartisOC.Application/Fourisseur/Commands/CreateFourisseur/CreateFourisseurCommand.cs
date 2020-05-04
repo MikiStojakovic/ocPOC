22be699dcc7ac517d5857d885c79fda9678e9be6
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-04 15:48:04 +0200

    Added CreateFourisseur functionality

---
+++ CopartisOC.Application/Fourisseur/Commands/CreateFourisseur/CreateFourisseurCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopartisOC.Application.Fourisseur.Commands.CreateFourisseur
+{
+    public class CreateFourisseurCommand : IRequest<Guid>
+    {
+        public Guid FourisseurId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CopartisOC.Application/Fourisseur/Commands/CreateFourisseur/CreateFourisseurCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using CopartisOC.Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CopartisOC.Application.Fourisseur.Commands.CreateFourisseur
+{
+    public class CreateFourisseurCommandValidator : AbstractValidator<CreateFourisseurCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateFourisseurCommandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+
+            RuleFor(f => f.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MustAsync(BeUniqueName).WithMessage("The specified title already exists.");
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return await _context.Fourisseurs
+                .AllAsync(f => f.Name != name);
+        }
+    }
+}
