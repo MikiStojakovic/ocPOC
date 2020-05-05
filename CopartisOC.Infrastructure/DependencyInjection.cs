7374db0152ae3d3dfe2bde7a19ae502a714e2dbd
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-05 16:24:21 +0200

    Added DependencyInjection file to CopartisOC.Infrastructure project

--- CopartisOC.Infrastructure/DependencyInjection.cs
+++ CopartisOC.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CopartisOC.Infrastructure
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            //{
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //        options.UseInMemoryDatabase("CleanArchitectureDb"));
+            //}
+            //else
+            //{
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //        options.UseSqlServer(
+            //            configuration.GetConnectionString("DefaultConnection"),
+            //            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            //}
+
+            //services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            //services.AddDefaultIdentity<ApplicationUser>()
+            //        .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddIdentityServer()
+            //    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+            //services.AddTransient<IDateTime, DateTimeService>();
+            //services.AddTransient<IIdentityService, IdentityService>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
     }
 }
