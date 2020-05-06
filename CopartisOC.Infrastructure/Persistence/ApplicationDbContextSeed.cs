5ed54ef152e13de7a85e3768d2d31aba4c0c9c26
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-06 17:13:58 +0200

    Added ApplicationDbContextSeed

---
+++ CopartisOC.Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using CopartisOC.Domain.Entities;
+using CopartisOC.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopartisOC.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedDefaultUserAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
+
+            if (userManager.Users.All(u => u.UserName != defaultUser.UserName))
+            {
+                await userManager.CreateAsync(defaultUser, "Administrator1!");
+            }
+        }
+
+        public static async Task SeedSampleDataAsync(ApplicationDbContext context)
+        {
+            // Seed, if necessary
+            if (!context.Fourisseurs.Any())
+            {
+                context.Fourisseurs.Add(new Fourisseur
+                {
+                    Name = "Copartis"
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
