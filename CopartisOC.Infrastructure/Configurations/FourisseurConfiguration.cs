1da65531a3b5408ff97f9744dfbcc2745f5c7aa4
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-07 17:32:45 +0200

    Added FourisseurConfiguration

---
+++ CopartisOC.Infrastructure/Configurations/FourisseurConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CopartisOC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopartisOC.Infrastructure.Configurations
+{
+    public class FourisseurConfiguration : IEntityTypeConfiguration<Fourisseur>
+    {
+        public FourisseurConfiguration()
+        {
+
+        }
+
+        public void Configure(EntityTypeBuilder<Fourisseur> builder)
+        {
+            builder.Property(f => f.Name)
+                .IsRequired();                
+        }
+    }
+}
