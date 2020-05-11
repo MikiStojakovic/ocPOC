3c72f48338e1977015e23b147d0e2b9d8440e00a
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-11 10:10:04 +0200

    Added DateTimeService

---
+++ CopartisOC.Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using CopartisOC.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopartisOC.Infrastructure.Services
+{
+    public class DateTimeService : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
