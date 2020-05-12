1d2ed2529f2ef2671c44c2967a3afb882b00c6cf
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-12 15:09:26 +0200

    Added FourisseurController

---
+++ CopartisOC.WebUI/Controllers/FourisseurController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CopartisOC.Application.Fourisseur.Commands.CreateFourisseur;
+using CopartisOC.Application.Fourisseur.Queries.GetFourisseurList;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopartisOC.WebUI.Controllers
+{
+    public class FourisseurController : ApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<FourisseurListVm>> GetAll()
+        {
+            var vm = await Mediator.Send(new GetFourisseurListQuery());
+
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create(CreateFourisseurCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
