ac245ff8b0e56bb089658298bf47532adb0e390d
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-14 17:18:25 +0200

    Added first unit test for statement function

--- CopartisOC.WebUI/ClientApp/refactoring-js-app/src/main.test.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app/src/main.test.js
@@ -1,1 +1,10 @@
 var statement = require('./main.js').statement;
+
+var invoice = require('./invoices.json');
+var plays = require('./plays.json');
+
+test('invalid statement test', () => {
+  expect(statement(invoice, plays)).toBe(
+    'Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n'
+  );
+});
