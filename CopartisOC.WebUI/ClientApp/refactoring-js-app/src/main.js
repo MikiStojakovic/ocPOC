8d9a7405704262cd7aaa61db2072ee601901d9ec
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-05-26 12:58:18 +0200

    Moved volumeCreditsFor function to intermediate data structure

--- CopartisOC.WebUI/ClientApp/refactoring-js-app/src/main.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app/src/main.js
@@ -11,6 +11,15 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
@@ -66,7 +75,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -77,14 +86,6 @@ function renderPlainText(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
