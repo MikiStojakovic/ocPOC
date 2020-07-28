822108cfc0086f0a990f0d4ab0b08e789732f484
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-07-28 16:20:01 +0200

    Split loop

--- CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
@@ -1,7 +1,16 @@
 function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
-  let totalAmount = opaHopa();
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    // print line for this order
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seats)\n`;
+  }
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
