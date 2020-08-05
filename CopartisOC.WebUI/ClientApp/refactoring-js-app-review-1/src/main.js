1f1f65ca25cc593a59a2b03d1936a76240942f45
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-08-05 16:59:56 +0200

    Extracted function - createdStatementData

--- CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
@@ -1,10 +1,15 @@
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  let statementData = createStatementData(invoice, plays);
   return renderPlainText(statementData, plays);
+
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
