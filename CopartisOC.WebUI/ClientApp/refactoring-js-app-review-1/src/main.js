0b41372667c333020866ef73196e8b4e9e0149a6
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-08-06 14:52:53 +0200

    Added function - renderHtml

--- CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/main.js
@@ -20,6 +20,21 @@ function renderPlainText(data) {
   return result;
 }
 
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>play</th><th>seats</th><th>costs</th></tr>`;
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(per.amount)}</td></tr>\n`;
+  }
+  result += `</table>\n`;
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
