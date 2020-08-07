57b8527d071e8a3a30adfcaa29ae6856d268d9c2
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-08-07 12:08:36 +0200

    Changed createPerformanceCalculator function - added switch statement

--- CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/createStatementData.js
+++ CopartisOC.WebUI/ClientApp/refactoring-js-app-review-1/src/createStatementData.js
@@ -42,7 +42,14 @@ function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
 }
 
 class PerformanceCalculator {
